dc40f57912ebb5ca8df0b10be29e7469942afb76
lucianeme1205 <2522026@ucc.edu.ar>
2026-09-21 08:42:21 -0300

    feach: ejercicio convertir grados a radianes

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -34,3 +34,23 @@ function convertirUnidades(id, Valor){
     }
 
 }
+/**
+ * Conversion de unidades de grados a radianes
+ * @method convertirGR
+ * @param {string} id . id del elemento input del html
+
+ */
+
+function convertirGR(id){
+    let grad, rad;
+
+    if(id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = grad*Math.PI/180;
+    }else{
+        rad = document.getElementById("radianes").value;
+        grad = rad*180/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
